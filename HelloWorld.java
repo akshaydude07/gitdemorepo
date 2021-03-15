818084e3e8df78205dabd8e48f34e9c05e3d55a9
adude <adude@tibco.com>
2021-03-15 12:27:59 +0530

    Adding hello world class

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+Class HelloWorld {
+	public static void main (String args []){
+		System.out.println("Hello to new Git World");
+		System.out.println("This is the first java class of this demo repo");
+	}
+}
