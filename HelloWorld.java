7a94a45532c8ee2fe432e8f2018605c2afd9d073
adude <adude@tibco.com>
2021-03-15 13:14:18 +0530

    Adding ignore file & modified class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-Class HelloWorld {
+class HelloWorld {
 	public static void main (String args []){
 		System.out.println("Hello to new Git World");
 		System.out.println("This is the first java class of this demo repo");
