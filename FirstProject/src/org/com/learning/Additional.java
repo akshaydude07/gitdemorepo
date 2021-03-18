80b1bd8cac982a9d0a5697d08e50a951b23cfbd7
akshaydude07 <80522943+akshaydude07@users.noreply.github.com>
2021-03-18 11:29:51 +0530

    Op changed
    Op changed

--- FirstProject/src/org/com/learning/Additional.java
+++ FirstProject/src/org/com/learning/Additional.java
@@ -3,7 +3,7 @@ class Additional{
 	int i1 =0;
 	int j1 =1;
 	public void results(){
-		System.out.println("In Addition " + (i1 + j1));
+		System.out.println("In substraction " + (i1 - j1));
 	}
 	
 }
