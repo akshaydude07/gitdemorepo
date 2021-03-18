c13e84db5acd04acd27dcb487b255701174f3011
akshaydude07 <80522943+akshaydude07@users.noreply.github.com>
2021-03-18 11:30:43 +0530

    Divide Op

--- FirstProject/src/org/com/learning/Additional.java
+++ FirstProject/src/org/com/learning/Additional.java
@@ -1,9 +1,9 @@
 package org.com.learning;
 class Additional{
-	int i1 =0;
-	int j1 =1;
+	int i2 =0;
+	int j2 =1;
 	public void results(){
-		System.out.println("In Addition " + (i1 + j1));
+		System.out.println("In Divide " + (i2 / j2));
 	}
 	
 }
