b21f3243ce6f18413e1e96c51f6552e6e8cf4982
akshaydude07 <80522943+akshaydude07@users.noreply.github.com>
2021-03-18 11:26:39 +0530

    chanegs done

--- FirstProject/src/org/com/learning/Additional.java
+++ FirstProject/src/org/com/learning/Additional.java
@@ -1,10 +1,9 @@
 package org.com.learning;
 class Additional{
-	int i =0;
-	int j =1;
-	int result = i+j;
-	void results(){
-		System.out.println("Addition " + result);	
+	int i1 =0;
+	int j1 =1;
+	public void results(){
+		System.out.println("In Addition " + (i1 + j1));
 	}
 	
 }
