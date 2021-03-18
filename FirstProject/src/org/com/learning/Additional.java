8b2e1718cf7bd90d0cd0dca6d0a863e3fa7be494
akshaydude07 <80522943+akshaydude07@users.noreply.github.com>
2021-03-18 11:23:02 +0530

    Modified Operation

--- FirstProject/src/org/com/learning/Additional.java
+++ FirstProject/src/org/com/learning/Additional.java
@@ -2,5 +2,9 @@ package org.com.learning;
 class Additional{
 	int i =0;
 	int j =1;
-	//System.out.println("In Additional");
+	int result = i+j;
+	void results(){
+		System.out.println("Addition " + result);	
+	}
+	
 }
